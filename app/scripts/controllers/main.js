043f1fe3cb32896bd338f6a63054f3857acd1572
Victoria Thai <vickytoria.thai@gmail.com>
2015-01-30 02:37:50 -0600

    Yo Man Setup

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name vickdbApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the vickdbApp
+ */
+angular.module('vickdbApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
